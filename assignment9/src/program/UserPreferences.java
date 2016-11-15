20c965972563296be6ccab01aea1d7197af898ac
Mark <ericmw02@students.ipfw.edu>
2016-11-15 09:44:20 -0500

    User setting info

---
+++ assignment9/src/program/UserPreferences.java
@@ -0,0 +1,16 @@
+package program;
+
+/**
+ * @author Mark
+ * This class accesses userSetting.dat to 
+ * 		1- test if user has a folder to store reports
+ * 		2- ask user for file save folder if none exists
+ * 		3- provide folder location to be able to save screenings 
+ */
+public class UserPreferences {
+
+	public UserPreferences() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
