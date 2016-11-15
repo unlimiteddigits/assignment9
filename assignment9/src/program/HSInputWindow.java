f4a2eaf3801613941344bc580bfccae823ccca56
Mark <ericmw02@students.ipfw.edu>
2016-11-15 09:31:47 -0500

    Built some infrastructure

---
+++ assignment9/src/program/HSInputWindow.java
@@ -0,0 +1,69 @@
+package program;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class HSInputWindow extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			HSInputWindow dialog = new HSInputWindow();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public HSInputWindow() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblPatientName = new JLabel("Patient Name");
+			lblPatientName.setBounds(10, 11, 76, 14);
+			contentPanel.add(lblPatientName);
+		}
+		{
+			textField = new JTextField();
+			textField.setBounds(106, 8, 139, 20);
+			contentPanel.add(textField);
+			textField.setColumns(10);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
--- assignment9/src/program/HealthScreening.java
+++ assignment9/src/program/HealthScreening.java
@@ -2,4 +2,15 @@ package program;
 
 public class HealthScreening {
 
+	public HealthScreening() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HSInputWindow UserInput;
+		UserInput = new HSInputWindow();
+		UserInput.setVisible(true);
+	}
+
 }
