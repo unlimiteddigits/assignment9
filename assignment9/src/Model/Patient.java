1556078212f46fd8079ea5cecd9351de5778fa9f
Mark <ericmw02@students.ipfw.edu>
2016-11-16 14:27:13 -0500

    possible name

---
+++ assignment9/src/Model/Patient.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Patient {
+
+	public Patient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
