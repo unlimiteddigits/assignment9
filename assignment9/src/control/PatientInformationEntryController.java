0a2a2880e4449d688cc5a8b970d2f3094609b2ab
icesg01 <icestanley@gmail.com>
2016-12-01 09:53:31 -0500

    moved the actual creation of the search by date frame
    temporary,this should go into another view class which will be
    controlled by a different model

--- assignment9/src/control/PatientInformationEntryController.java
+++ assignment9/src/control/PatientInformationEntryController.java
@@ -2,15 +2,23 @@ package control;
 
 
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.rmi.RemoteException;
 import java.text.DecimalFormat;
+
+import org.json.JSONException;
 
 import com.beetledev.www.BmiServiceSoapProxy;
 import com.beetledev.www.ConverterServiceSoapProxy;
@@ -39,7 +47,133 @@ public class PatientInformationEntryController {
 		view.addSearchByDateActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				view.btnSearchByDate(e);
+				javax.swing.JFrame searchDateFrame = new javax.swing.JFrame();
+
+				searchDateFrame.setSize(new Dimension(500, 500));
+
+				searchDateFrame.setLocationRelativeTo(null);
+				searchDateFrame.setResizable(false);
+
+				javax.swing.JTextArea dbResults = new javax.swing.JTextArea(5, 30);
+
+				dbResults.setEditable(false);
+
+				javax.swing.JPanel NorthPanel = new javax.swing.JPanel();
+
+				NorthPanel.setLayout(new GridBagLayout());
+
+				javax.swing.JLabel newLabel = new javax.swing.JLabel();
+				org.jdesktop.swingx.JXDatePicker datePicker = new org.jdesktop.swingx.JXDatePicker();
+				javax.swing.JButton btnSearch = new javax.swing.JButton();
+
+				GridBagConstraints gbc = new GridBagConstraints();
+
+				gbc.gridx = GridBagConstraints.RELATIVE;
+
+				gbc.gridy = 0;
+
+				gbc.weightx = .1;
+
+				gbc.weighty = 1;
+
+				NorthPanel.add(newLabel, gbc);
+
+				gbc.fill = GridBagConstraints.BOTH;
+
+				gbc.gridx = GridBagConstraints.RELATIVE;
+				gbc.gridy = 0;
+				gbc.insets = new Insets(1, 1, 1, 20);
+				gbc.weightx = 1;
+				gbc.weighty = .7;
+
+				NorthPanel.add(datePicker, gbc);
+
+				gbc.fill = GridBagConstraints.HORIZONTAL;
+				gbc.gridx = GridBagConstraints.RELATIVE;
+				gbc.gridy = 0;
+				gbc.weightx = .2;
+				gbc.weighty = 1;
+
+				NorthPanel.add(btnSearch, gbc);
+
+				btnSearch.setText("search records");
+
+				datePicker.requestFocusInWindow();
+				newLabel.setText("Select Date: ");
+
+				class MyAdapter implements ActionListener, KeyListener {
+
+					@Override
+					public void keyPressed(KeyEvent evt) {
+
+						DataBase search = new DataBase(null);
+
+						try {
+							dbResults.setText(search.search(0, datePicker.getDate().toString()));
+
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+
+					@Override
+					public void keyReleased(KeyEvent e) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void keyTyped(KeyEvent e) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void actionPerformed(ActionEvent evt) {
+						DataBase search = new DataBase(null);
+
+						try {
+							dbResults.setText(search.search(0, datePicker.getDate().toString()));
+
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+
+				}
+
+				MyAdapter adapter = new MyAdapter();
+
+				datePicker.addActionListener(adapter);
+				btnSearch.addActionListener(adapter);
+
+				searchDateFrame.getRootPane().setDefaultButton(btnSearch);
+
+				
+				
+				
+				
+				javax.swing.JScrollPane records = new javax.swing.JScrollPane(dbResults);
+
+				
+			
+				
+				records.setSize(new Dimension(200, 200));
+
+				searchDateFrame.add(NorthPanel, BorderLayout.NORTH);
+				searchDateFrame.add(records, BorderLayout.CENTER);
+				searchDateFrame.setTitle("Search Records by Date");
+
+				searchDateFrame.setVisible(true);
+			
+				
+				
+				
+				
 			}
 		});
 		
