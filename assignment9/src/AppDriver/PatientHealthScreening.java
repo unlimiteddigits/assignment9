ba6e54d40a73e0a4f457b16b02128e3f11c405b5
Mark <ericmw02@students.ipfw.edu>
2016-12-03 15:17:50 -0500

    Created AppDriver Package and a new driver so we don't turn in "TestDriver"

---
+++ assignment9/src/AppDriver/PatientHealthScreening.java
@@ -0,0 +1,28 @@
+package AppDriver;
+
+import Model.PatientRecord;
+import View.HSInputWindow;
+import control.PatientInformationEntryController;
+
+public class PatientHealthScreening {
+
+	public static void main(String[] args) {
+		
+		PatientRecord thisModel;
+		HSInputWindow thisView;
+		try {
+			thisModel = new PatientRecord("John");
+			
+			thisView = new HSInputWindow();
+			PatientInformationEntryController controller = new PatientInformationEntryController(thisView,thisModel);
+		
+			thisView.setVisible(true);
+				
+				} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
