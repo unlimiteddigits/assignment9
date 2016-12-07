6c04b159d43117afbc43cb6a4b79f7aff8d40b02
icesg01 <icestanley@gmail.com>
2016-12-06 19:34:06 -0500

    I added comments to the patientRecord class

--- assignment9/src/Model/PatientRecord.java
+++ assignment9/src/Model/PatientRecord.java
@@ -25,15 +25,24 @@ public class PatientRecord {
     
  
   
-
+/**
+ * Constructor, creates a new patient record 
+ * 
+ */
     public PatientRecord(final String name){
         this.name = name;
     }
-    
+    /**
+     * Sets the name of the patient
+     * @param name
+     */
    public void setName(final String name){
 	   this.name = name;
    }
-    
+    /**
+     * Sets the age of the patient
+     * @param age
+     */
     public void setAge(final String age){
         this.age = age;
     }
@@ -48,73 +57,125 @@ public class PatientRecord {
     
    
     
-  
+  /**
+   * Sets the blood pressure of the patient
+   * @param bp
+   */
     
     public void setBloodPressure(final String bp){
         this.bloodPressure = bp;
     }
     
+    /**
+     * Sets the blood pressure risk for the patient
+     * @param bpr
+     */
     public void setBloodPressureRisk(final String bpr){
     	bloodPressureRisk = bpr;
     }
-    
+    /**
+     * Sets the feet of the patient
+     * @param feet
+     */
     public void setHeightFeet(final String feet){
         this.heightFeet = feet;
     }
-    
+    /**
+     * Sets the inches of the patient
+     * @param inches
+     */
     public void setHeightInches(final String inches){
     	this.heightInches = inches;
     }
-    
+    /**
+     * Sets the weight of the patient
+     * @param weight
+     */
     public void setWeight(final String weight){
         this.weight = weight;
     }
-    
+    /**
+     * Sets the cholesterol of the patient
+     * @param cholesterol
+     */
     public void setCholesterol(final String cholesterol){
     	this.cholesterol = cholesterol;
     }
-    
+    /**
+     * Sets the cholesterol risk of the patient
+     * @param cholesterolRisk
+     */
     public void setCholesterolRisk(final String cholesterolRisk){
         this.cholesterolRisk = cholesterolRisk;
     }
-    
+   /**
+    * Sets the bmi of the patient 
+    * @param bmi
+    */
     public void setBMI(final String bmi){
         this.bmi = bmi;
     }
-    
+    /**
+     * Sets the bmi risk of the patient
+     * @param bmiRisk
+     */
     public void setBmiRisk(final String bmiRisk){
     	this.bmiRisk = bmiRisk;
     }
-    
+    /**
+     * Sets the date the patient record was recorded
+     * @param date
+     */
     public void setDate(final String date){
         this.date = date;
     }
-    
+    /**
+     * Returns the date the patient record was recorded
+     * @return
+     */
     public String getDate(){
     	return date;
     }
-  
+  /**
+   * Returns the age of the patient
+   * @return
+   */
     public String getAge(){
     	return age;
     }
-    
+/**
+ * Returns the name of the patient    
+ * @return
+ */
     public String getName(){
     	return name;
     }
-    
+    /**
+     * Returns the patients feet value
+     * @return
+     */
     public String getHeightFeet(){
     	return heightFeet;
     }
-    
+    /**
+     * returns the patients inches value
+     * @return
+     */
     public String getHeightInches(){
     	return heightInches;
     }
     
-    
+    /**
+     * Returns the patients blood pressure
+     * @return
+     */
     public String getBloodPressure(){
     	return bloodPressure;
     }
-    
+    /**
+     * Returns the patients cholesterol value
+     * @return
+     */
     public String getCholesterol(){
     	return cholesterol;
     }
