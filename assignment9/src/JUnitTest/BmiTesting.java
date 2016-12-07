c7daa1cf43319a92958d1f2970309c54bc337db1
Mark <ericmw02@students.ipfw.edu>
2016-12-07 10:58:33 -0500

    Added jUnitTest

---
+++ assignment9/src/JUnitTest/BmiTesting.java
@@ -0,0 +1,14 @@
+package jUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BmiTesting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
